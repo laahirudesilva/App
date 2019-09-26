9c886a4bae47f343b5d1567f69d8fb723dab743c
laahirudesilva <mlsdesilva10@gmail.com>
2019-09-26 19:53:16 +0530

    restarted

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,16 +1,10 @@
 import React, { Component } from 'react';
-import { Container } from 'semantic-ui-react';
-import EventDashboard from '../../features/event/EventDashboard/EventDashboard';
-import NavBar from '../../features/nav/NavBar/NavBar';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <NavBar />
-        <Container className="main">
-          <EventDashboard />
-        </Container>
+        <h1>Re-vents</h1>
       </div>
     );
   }
